e06706393c618d3e716b6f74ec51a325420a448b
James Stone <james@jamesstone.co>
2017-03-02 15:29:31 -0800

    updated js

--- src/js/app.js
+++ src/js/app.js
@@ -1,12 +1,18 @@
-jQuery(document).ready(function() {
-	jQuery(document).foundation();
-});
-
-// home page hero button scroll interaction
-jQuery(document).ready(function() {
-  jQuery("#js-trigger-work").on('click', function() {
-    jQuery('html, body').animate({
+$(document).foundation();
+/* home page hero button scroll interaction */
+$(document).ready(function() {
+  $("#js-trigger-work").on('click', function() {
+    $('html, body').animate({
       scrollTop: $( $("#js-marker-work")).offset().top
     }, 1200);
   })
 });
+// jQuery(document).ready(function() {
+//   jQuery(document).foundation();
+//   /* home page hero button scroll interaction */
+//   jQuery("#js-trigger-work").on('click', function() {
+//     jQuery('html, body').animate({
+//       scrollTop: $( $("#js-marker-work")).offset().top
+//     }, 1200);
+//   })
+// });
